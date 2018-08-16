fd569948e34fc2b0ecd1a08c7f068690ee5ef0bb
Bryn <bryn@Bryns-MacBook-Pro.local>
2018-08-15 21:32:22 -0500

    Adding stateParams to weather service to reference lat and lon

--- src/app/core/weather/weather.service.js
+++ src/app/core/weather/weather.service.js
@@ -1,13 +1,17 @@
 'use strict';
 
 angular.module('core.weather').factory('Weather', [
-  '$resource',
-  function($resource) {
+  '$resource', '$stateParams',
+  function($resource, $stateParams) {
     let weatherResource;
+      console.log("weatherResource request", $stateParams.lat, $stateParams.lon);
 
     weatherResource = $resource(
       `https://api.openweathermap.org/data/2.5/forecast/daily?cnt=4&&units=imperial&APPID=cd12cb61c3a3512e0ceffa3c71fec447`,
-      {},
+      {
+        lat: $stateParams.lat, 
+        lon: $stateParams.lon
+      },
       {
         getAll: {
           method: 'GET'
